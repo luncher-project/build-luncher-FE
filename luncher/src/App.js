ec3758266e8d31d76a24d106b70e6ef6bbde52ca
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-15 14:01:58 -0400

    created sign up component, rendering to screen, working on routes

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+//add router//
+import { Route, NavLink, Link } from 'react-router-dom';
+
+//add components//
 import LogIn from './components/login/LogIn';
+import SignUp from './components/signup/SignUp';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div className='nav-bar'>
+      <NavLink exact to='/login'>LogIn</NavLink>
+      <NavLink exact to='/signup'>SignUp</NavLink>
+      </div>
        <LogIn />
+       <SignUp />
        </div>
     );
   }
