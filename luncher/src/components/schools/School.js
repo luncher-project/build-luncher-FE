463f7722262848ecabe332e5bdd391a2178a75f7
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-18 19:09:55 -0400

    styling school list

--- luncher/src/components/schools/School.js
+++ luncher/src/components/schools/School.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 const School = props => {
     return(
-        <div className='school-info'>
-            <h2>{props.schoolName}</h2>
-            <p>{props.state}</p>
-            <p>{props.zip}</p>
-            <p>{props.fundsNeeded}</p>
-            <p>{props.contact}</p>
+        <div className='container'>
+        <Card className='school-info'>
+            <CardHeader>{props.schoolName}</CardHeader>
+            <CardContent><strong>State:</strong> {props.state}</CardContent>
+            <CardContent><strong>Zip:</strong> {props.zip}</CardContent>
+            <CardContent><strong>Funds Needed:</strong> {props.fundsNeeded}</CardContent>
+            <CardContent><strong>Contact:</strong> {props.contact}</CardContent>
+            <Button variant='outlined'>Ready to Donate?</Button>
+        </Card>
         </div>
     );
 }
